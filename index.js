5e68e7211366fb9cf17f64988574f71283ae9983
Aika122 <yamashitaaika0122@gmail.com>
2024-11-17 16:45:58 +0900

    マウスのクリック検出を追加

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ const puzzlePositions = Array.from({ length: 16 }, () => ({
   top: Math.floor(Math.random() * 450)
 }));
 
-// 各ユーザーのカーソル位置情報を保持
+// 各ユーザーのカーソル位置情報とクリック状態を保持
 let cursors = {};
+let clickStates = {}; // ピースごとにクリックしているユーザーを追跡
 
 // サーバーの起動
 http.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log('Initial puzzle positions:', puzzlePositions);
 });
 
 // ソケット接続の処理
@@ -34,50 +34,9 @@ io.on('connection', (socket) => {
   // 接続時に初期位置情報を送信
   socket.emit('initialize puzzle', puzzlePositions);
 
-  let participantNumber = null;
-  let participantLetter = null;
-
   // スタートボタンを押されたときの処理
   socket.on('start game', (data) => {
-    participantNumber = data.participantNumber;
-    participantLetter = data.participantLetter;
-
     io.emit('game started');
-
-    // ゲームのログファイル名を作成
-    socket.gameLogFile = `game_log_${participantNumber}_${participantLetter}.csv`;
-    socket.interactionLogFile = `interaction_log_${participantNumber}_${participantLetter}.csv`;
-
-    // ゲームログとインタラクションログの初期設定（ヘッダー）
-    const gameLogHeader = 'Event,Duration (ms)\n';
-    const interactionLogHeader = 'Piece,Interaction Duration (ms)\n';
-
-    // ファイルにヘッダーを書き込む
-    fs.writeFileSync(path.join(__dirname, socket.gameLogFile), gameLogHeader);
-    fs.writeFileSync(path.join(__dirname, socket.interactionLogFile), interactionLogHeader);
-  });
-
-  // ピースの移動イベント
-  socket.on('piece move', (data) => {
-    puzzlePositions[data.index] = { left: data.left, top: data.top }; // サーバーの位置情報を更新
-    io.emit('piece move', data); // 全クライアントに更新情報を送信
-  });
-
-  // ピースのスナップイベント
-  socket.on('piece snap', (data) => {
-    puzzlePositions[data.index] = { left: data.left, top: data.top }; // サーバーの位置情報を更新
-    io.emit('piece snap', data);
-  });
-
-  // インタラクション時間のログ保存
-  socket.on('log interaction', (data) => {
-    console.log(`Interaction received for piece ${data.index} with duration ${data.duration} ms`);
-
-    const logEntry = `Piece ${data.index},${data.duration}\n`;
-    fs.appendFile(path.join(__dirname, socket.interactionLogFile), logEntry, (err) => {
-      if (err) throw err;
-      console.log('Interaction data saved.');
-    });
   });
 
   // カーソル位置の更新
@@ -86,21 +45,48 @@ io.on('connection', (socket) => {
     io.emit('mouse move', { id: socket.id, position: data });
   });
 
-  // ゲーム終了の処理
-  socket.on('game finished', (data) => {
-    console.log(`Game finished with duration: ${data.duration} ms, Total Move Time: ${data.totalMoveTime} ms, Total Click Time: ${data.totalClickTime} ms`);
+  // ピースのドラッグ開始
+  socket.on('start drag', (data) => {
+    if (!clickStates[data.index]) {
+      clickStates[data.index] = new Set();
+    }
+    clickStates[data.index].add(socket.id);
 
-    const logEntry = `Game Finished,${data.duration}\nTotal Move Time,${data.totalMoveTime}\nTotal Click Time,${data.totalClickTime}\n`;
-    fs.appendFile(path.join(__dirname, socket.gameLogFile), logEntry, (err) => {
-      if (err) throw err;
-      console.log('Game finished data saved.');
-    });
+    // 2人が同じピースをクリックしているか確認
+    if (clickStates[data.index].size >= 2) {
+      io.emit('allow move', data.index);
+    }
+  });
+
+  // ピースのドラッグ終了
+  socket.on('end drag', (data) => {
+    if (clickStates[data.index]) {
+      clickStates[data.index].delete(socket.id);
+      if (clickStates[data.index].size < 2) {
+        io.emit('stop move', data.index);
+      }
+    }
+  });
+
+  // ピースの移動イベント
+  socket.on('piece move', (data) => {
+    puzzlePositions[data.index] = { left: data.left, top: data.top }; // サーバーの位置情報を更新
+    io.emit('piece move', data); // 全クライアントに更新情報を送信
   });
 
   // ユーザーが切断されたときの処理
   socket.on('disconnect', () => {
     console.log('A user disconnected');
-    delete cursors[socket.id]; // カーソル情報の削除
-    io.emit('user disconnected', { id: socket.id });
+    delete cursors[socket.id];
+
+    // すべてのクリック状態から切断されたユーザーを削除
+    Object.keys(clickStates).forEach((index) => {
+      if (clickStates[index].has(socket.id)) {
+        clickStates[index].delete(socket.id);
+        if (clickStates[index].size < 2) {
+          io.emit('stop move', parseInt(index));
+        }
+      }
+    });
   });
 });
