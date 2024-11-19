07156fd09ba166d42e3497aa4fa3fd2ee760f2c2
Aika122 <yamashitaaika0122@gmail.com>
2024-11-19 16:37:37 +0900

    フレームを左上に

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// 必要なモジュールのインポート
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const fs = require('fs');
-const path = require('path');
 
 // ポート設定
 const PORT = process.env.PORT || 10000;
@@ -14,14 +11,17 @@ app.use(express.static('public'));
 
 // 初期パズル位置情報を保持
 const puzzlePositions = Array.from({ length: 16 }, () => ({
-  left: Math.floor(Math.random() * 450), // 450px以内に収まるように設定
-  top: Math.floor(Math.random() * 450)
+  left: Math.floor(Math.random() * 600), // 600px以内に収まるように設定
+  top: Math.floor(Math.random() * 600)
 }));
+
+// 各ユーザーのカーソル位置情報とクリック状態を保持
+let cursors = {};
+let clickStates = {};
 
 // サーバーの起動
 http.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log('Initial puzzle positions:', puzzlePositions); // サーバー起動後に初期位置を出力
 });
 
 // ソケット接続の処理
@@ -31,70 +31,62 @@ io.on('connection', (socket) => {
   // 接続時に初期位置情報を送信
   socket.emit('initialize puzzle', puzzlePositions);
 
-  let participantNumber = null;
-  let participantLetter = null;
+  // ピースのドラッグ開始
+  socket.on('start drag', (data) => {
+    if (!clickStates[data.index]) {
+      clickStates[data.index] = new Set();
+    }
+    clickStates[data.index].add(socket.id);
 
-  // スタートボタンを押されたときの処理
-  socket.on('start game', (data) => {
-    participantNumber = data.participantNumber;
-    participantLetter = data.participantLetter;
+    console.log(`Piece ${data.index} is being dragged by: ${[...clickStates[data.index]].join(', ')}`);
 
-    io.emit('game started');
+    // 1人以上がクリックしている場合に移動を許可
+    if (clickStates[data.index].size >= 1) {
+      console.log(`Piece ${data.index} move allowed`);
+      io.emit('allow move', data.index);
+    }
+  });
 
-    // ゲームのログファイル名を作成
-    socket.gameLogFile = `game_log_${participantNumber}_${participantLetter}.csv`;
-    socket.interactionLogFile = `interaction_log_${participantNumber}_${participantLetter}.csv`;
+  // ピースのドラッグ終了
+  socket.on('end drag', (data) => {
+    if (clickStates[data.index]) {
+      clickStates[data.index].delete(socket.id);
+      console.log(`Piece ${data.index} is no longer dragged by: ${socket.id}`);
 
-    // ゲームログとインタラクションログの初期設定（ヘッダー）
-    const gameLogHeader = 'Event,Duration (ms)\n';
-    const interactionLogHeader = 'Piece,Interaction Duration (ms)\n';
-
-    // ファイルにヘッダーを書き込む
-    fs.writeFileSync(path.join(__dirname, socket.gameLogFile), gameLogHeader, (err) => {
-      if (err) throw err;
-    });
-
-    fs.writeFileSync(path.join(__dirname, socket.interactionLogFile), interactionLogHeader, (err) => {
-      if (err) throw err;
-    });
+      // どちらかがクリックをやめた場合にピースの移動を停止
+      if (clickStates[data.index].size < 1) {
+        console.log(`Piece ${data.index} move stopped`);
+        io.emit('stop move', data.index);
+      }
+    }
   });
 
   // ピースの移動イベント
   socket.on('piece move', (data) => {
     puzzlePositions[data.index] = { left: data.left, top: data.top }; // サーバーの位置情報を更新
-    socket.broadcast.emit('piece move', data); // 他のクライアントに更新情報を送信
+    console.log(`Piece ${data.index} moved to: left=${data.left}, top=${data.top}`);
+    io.emit('piece move', data); // 全クライアントに更新情報を送信
   });
 
-  // ピースのスナップイベント
-  socket.on('piece snap', (data) => {
-    puzzlePositions[data.index] = { left: data.left, top: data.top }; // サーバーの位置情報を更新
-    socket.broadcast.emit('piece snap', data);
-  });
-
-  // インタラクション時間のログ保存
-  socket.on('log interaction', (data) => {
-    console.log(`Interaction received for piece ${data.index} with duration ${data.duration} ms`);
-
-    const logEntry = `Piece ${data.index},${data.duration}\n`;
-    fs.appendFile(path.join(__dirname, socket.interactionLogFile), logEntry, (err) => {
-      if (err) throw err;
-      console.log('Interaction data saved.');
-    });
-  });
-
-  // ゲーム終了の処理
-  socket.on('game finished', (data) => {
-    console.log(`Game finished with duration: ${data.duration} ms, Total Move Time: ${data.totalMoveTime} ms, Total Click Time: ${data.totalClickTime} ms`);
-
-    const logEntry = `Game Finished,${data.duration}\nTotal Move Time,${data.totalMoveTime}\nTotal Click Time,${data.totalClickTime}\n`;
-    fs.appendFile(path.join(__dirname, socket.gameLogFile), logEntry, (err) => {
-      if (err) throw err;
-      console.log('Game finished data saved.');
-    });
+  // カーソル位置の更新
+  socket.on('mouse move', (data) => {
+    cursors[socket.id] = data;
+    io.emit('mouse move', { id: socket.id, position: data });
   });
 
   // ユーザーが切断されたときの処理
   socket.on('disconnect', () => {
     console.log('A user disconnected');
+    delete cursors[socket.id];
+
+    Object.keys(clickStates).forEach((index) => {
+      if (clickStates[index].has(socket.id)) {
+        clickStates[index].delete(socket.id);
+        if (clickStates[index].size < 1) {
+          console.log(`Piece ${index} move stopped due to disconnection`);
+          io.emit('stop move', parseInt(index));
+        }
+      }
+    });
   });
 });
