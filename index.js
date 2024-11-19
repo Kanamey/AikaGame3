52202381764c876e9a264ed10c375619d5b5c879
Aika122 <yamashitaaika0122@gmail.com>
2024-11-19 18:52:42 +0900

    二人が各々遊べる

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// 必要なモジュールのインポート
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const fs = require('fs');
+const path = require('path');
 
 // ポート設定
 const PORT = process.env.PORT || 10000;
@@ -11,82 +14,62 @@ app.use(express.static('public'));
 
 // 初期パズル位置情報を保持
 const puzzlePositions = Array.from({ length: 16 }, () => ({
-  left: Math.floor(Math.random() * 600), // 600px以内に収まるように設定
-  top: Math.floor(Math.random() * 600)
+  left: Math.floor(Math.random() * 450),
+  top: Math.floor(Math.random() * 450)
 }));
-
-// 各ユーザーのカーソル位置情報とクリック状態を保持
-let cursors = {};
-let clickStates = {};
 
 // サーバーの起動
 http.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log('Initial puzzle positions:', puzzlePositions);
 });
 
 // ソケット接続の処理
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // 接続時に初期位置情報を送信
   socket.emit('initialize puzzle', puzzlePositions);
 
-  // ピースのドラッグ開始
-  socket.on('start drag', (data) => {
-    if (!clickStates[data.index]) {
-      clickStates[data.index] = new Set();
+  socket.on('start game', (data) => {
+    console.log(`Game started by: ${data.participantNumber}${data.participantLetter}`);
+    if (data.participantLetter === 'watch') {
+      socket.emit('watch only');
+    } else {
+      io.emit('game started');
     }
-    clickStates[data.index].add(socket.id);
 
-    console.log(`Piece ${data.index} is being dragged by: ${[...clickStates[data.index]].join(', ')}`);
-
-    // 1人以上がクリックしている場合に移動を許可
-    if (clickStates[data.index].size >= 1) {
-      console.log(`Piece ${data.index} move allowed`);
-      io.emit('allow move', data.index);
-    }
+    socket.gameLogFile = `game_log_${data.participantNumber}_${data.participantLetter}.csv`;
+    fs.writeFileSync(socket.gameLogFile, 'Event,Duration (ms)\n', (err) => {
+      if (err) throw err;
+    });
   });
 
-  // ピースのドラッグ終了
-  socket.on('end drag', (data) => {
-    if (clickStates[data.index]) {
-      clickStates[data.index].delete(socket.id);
-      console.log(`Piece ${data.index} is no longer dragged by: ${socket.id}`);
-
-      // どちらかがクリックをやめた場合にピースの移動を停止
-      if (clickStates[data.index].size < 1) {
-        console.log(`Piece ${data.index} move stopped`);
-        io.emit('stop move', data.index);
-      }
-    }
-  });
-
-  // ピースの移動イベント
   socket.on('piece move', (data) => {
-    puzzlePositions[data.index] = { left: data.left, top: data.top }; // サーバーの位置情報を更新
-    console.log(`Piece ${data.index} moved to: left=${data.left}, top=${data.top}`);
-    io.emit('piece move', data); // 全クライアントに更新情報を送信
+    puzzlePositions[data.index] = { left: data.left, top: data.top };
+    socket.broadcast.emit('piece move', data);
   });
 
-  // カーソル位置の更新
-  socket.on('mouse move', (data) => {
-    cursors[socket.id] = data;
-    io.emit('mouse move', { id: socket.id, position: data });
+  socket.on('piece snap', (data) => {
+    puzzlePositions[data.index] = { left: data.left, top: data.top };
+    socket.broadcast.emit('piece snap', data);
   });
 
-  // ユーザーが切断されたときの処理
+  socket.on('log interaction', (data) => {
+    const logEntry = `Piece ${data.index},${data.duration}\n`;
+    fs.appendFile(socket.gameLogFile, logEntry, (err) => {
+      if (err) throw err;
+    });
+  });
+
+  socket.on('game finished', (data) => {
+    const logEntry = `Game Finished,${data.duration}\nTotal Move Time,${data.totalMoveTime}\n`;
+    fs.appendFile(socket.gameLogFile, logEntry, (err) => {
+      if (err) throw err;
+      console.log('Game finished data saved.');
+    });
+  });
+
   socket.on('disconnect', () => {
     console.log('A user disconnected');
-    delete cursors[socket.id];
-
-    Object.keys(clickStates).forEach((index) => {
-      if (clickStates[index].has(socket.id)) {
-        clickStates[index].delete(socket.id);
-        if (clickStates[index].size < 1) {
-          console.log(`Piece ${index} move stopped due to disconnection`);
-          io.emit('stop move', parseInt(index));
-        }
-      }
-    });
   });
 });
