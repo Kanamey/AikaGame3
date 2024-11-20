8855db4374f9aabee7d057d8e655879c435e0fea
Aika122 <yamashitaaika0122@gmail.com>
2024-11-20 15:21:47 +0900

    片方が正解するともう片方がロックされる？

--- index.js
+++ index.js
@@ -28,8 +28,10 @@ http.listen(PORT, () => {
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  // 初期パズル位置情報を送信
   socket.emit('initialize puzzle', puzzlePositions);
 
+  // ゲームスタートイベント
   socket.on('start game', (data) => {
     console.log(`Game started by: ${data.participantNumber}${data.participantLetter}`);
     if (data.participantLetter === 'watch') {
@@ -38,22 +40,35 @@ io.on('connection', (socket) => {
       io.emit('game started');
     }
 
+    // ゲームログファイルの作成
     socket.gameLogFile = `game_log_${data.participantNumber}_${data.participantLetter}.csv`;
     fs.writeFileSync(socket.gameLogFile, 'Event,Duration (ms)\n', (err) => {
       if (err) throw err;
     });
   });
 
+  // ピースの移動イベント
   socket.on('piece move', (data) => {
     puzzlePositions[data.index] = { left: data.left, top: data.top };
     socket.broadcast.emit('piece move', data);
   });
 
+  // ピースが正しい位置にスナップされた場合のイベント
   socket.on('piece snap', (data) => {
-    puzzlePositions[data.index] = { left: data.left, top: data.top };
-    socket.broadcast.emit('piece snap', data);
+    // サーバー側でピースの位置を確定
+    const correctX = (data.index % 4) * 150;
+    const correctY = Math.floor(data.index / 4) * 150;
+    puzzlePositions[data.index] = { left: correctX, top: correctY };
+
+    // 全てのクライアントにピースのスナップを通知
+    io.emit('piece snap', { index: data.index, left: correctX, top: correctY });
+    console.log(`Piece ${data.index} snapped to correct position at (${correctX}, ${correctY})`);
+
+    // ロックするのは正解したピースのみ
+    io.emit('lock piece', { index: data.index });
   });
 
+  // インタラクションのログ記録
   socket.on('log interaction', (data) => {
     const logEntry = `Piece ${data.index},${data.duration}\n`;
     fs.appendFile(socket.gameLogFile, logEntry, (err) => {
@@ -61,6 +76,7 @@ io.on('connection', (socket) => {
     });
   });
 
+  // ゲームが終了した際のログ記録
   socket.on('game finished', (data) => {
     const logEntry = `Game Finished,${data.duration}\nTotal Move Time,${data.totalMoveTime}\n`;
     fs.appendFile(socket.gameLogFile, logEntry, (err) => {
@@ -69,6 +85,7 @@ io.on('connection', (socket) => {
     });
   });
 
+  // ユーザーが切断された場合の処理
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
