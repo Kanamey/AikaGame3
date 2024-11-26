b93202aa5bddac6d7955cfc8223c09723f0935e8
Aika122 <yamashitaaika0122@gmail.com>
2024-11-26 19:04:50 +0900

    二人同時クリックのときだけ動かせる

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 二人同時に同じピースをクリックすると、右上に赤丸表示
+// 二人同時クリックのときだけ動かせる
 // 必要なモジュールのインポート
 const express = require('express');
 const app = express();
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
     currentlyClicked[data.index].push(socket.id);
 
     if (currentlyClicked[data.index].length === 2) {
-      io.emit('both clicked');
+      io.emit('both clicked', { index: data.index });
     }
   });
 
@@ -68,24 +68,26 @@ io.on('connection', (socket) => {
     if (currentlyClicked[data.index]) {
       currentlyClicked[data.index] = currentlyClicked[data.index].filter(id => id !== socket.id);
       if (currentlyClicked[data.index].length < 2) {
-        io.emit('not both clicked');
+        io.emit('not both clicked', { index: data.index });
       }
     }
   });
 
   // ピースの移動イベント
   socket.on('piece move', (data) => {
-    if (puzzlePositions[data.index].snapped) {
-      // すでにスナップされたピースは正しい位置に戻す
-      socket.emit('piece snap', {
-        index: data.index,
-        left: puzzlePositions[data.index].left,
-        top: puzzlePositions[data.index].top
-      });
-      return;
+    if (currentlyClicked[data.index] && currentlyClicked[data.index].length === 2) {
+      if (puzzlePositions[data.index].snapped) {
+        // すでにスナップされたピースは正しい位置に戻す
+        socket.emit('piece snap', {
+          index: data.index,
+          left: puzzlePositions[data.index].left,
+          top: puzzlePositions[data.index].top
+        });
+        return;
+      }
+      puzzlePositions[data.index] = { left: data.left, top: data.top, snapped: false };
+      socket.broadcast.emit('piece move', data);
     }
-    puzzlePositions[data.index] = { left: data.left, top: data.top, snapped: false };
-    socket.broadcast.emit('piece move', data);
   });
 
   // ピースが正しい位置にスナップされた場合のイベント
@@ -127,7 +129,7 @@ io.on('connection', (socket) => {
     for (let index in currentlyClicked) {
       currentlyClicked[index] = currentlyClicked[index].filter(id => id !== socket.id);
       if (currentlyClicked[index].length < 2) {
-        io.emit('not both clicked');
+        io.emit('not both clicked', { index: parseInt(index) });
       }
     }
   });
