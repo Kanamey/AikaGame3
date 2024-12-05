024a9d5771b25ea66a0618dcde2365eef2bf0f5f
Aika122 <yamashitaaika0122@gmail.com>
2024-12-05 19:18:10 +0900

    生成なし

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 二人別々に豆を二人で移動できる
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
@@ -7,15 +8,6 @@ const io = socketIo(server);
 
 app.use(express.static("public"));
 
-let beans = [];
-for (let i = 0; i < 5; i++) {
-    beans.push({
-        left: Math.random() * 500,
-        top: Math.random() * 500,
-        touchedBy: []  // プレイヤーIDを格納する配列
-    });
-}
-
 io.on("connection", (socket) => {
     console.log("A user connected");
 
@@ -24,55 +16,11 @@ io.on("connection", (socket) => {
 
     // 豆の移動情報を他のクライアントにブロードキャスト
     socket.on("beanMoved", (data) => {
-        beans[data.index].left = data.left;
-        beans[data.index].top = data.top;
-        io.emit("beanMoved", data); // 全てのクライアントにブロードキャスト
-    });
-
-    // 豆が触られた時の処理
-    socket.on("beanTouched", (index) => {
-        if (!beans[index].touchedBy.includes(socket.id)) {
-            beans[index].touchedBy.push(socket.id);
-        }
-
-        if (beans[index].touchedBy.length === 2) {
-            io.emit("beanColorChange", { index: index, color: "red" });
-        }
-    });
-
-    // 豆が放された時の処理
-    socket.on("beanReleased", (index) => {
-        beans[index].touchedBy = beans[index].touchedBy.filter(id => id !== socket.id);
-
-        if (beans[index].touchedBy.length < 2) {
-            io.emit("beanColorChange", { index: index, color: "brown" });
-        }
-    });
-
-    // 豆が削除されたことを他のクライアントに通知し、新しい豆を追加
-    socket.on("beanRemoved", (index) => {
-        beans[index] = null; // 一旦削除
-        io.emit("beanRemoved", index); // 全てのクライアントに削除情報をブロードキャスト
-
-        // 新しい豆を一定時間後に生成する
-        setTimeout(() => {
-            const newBean = {
-                left: Math.random() * 100,  // 左のお皿内のランダム位置に配置
-                top: Math.random() * 100,
-                touchedBy: []
-            };
-            beans[index] = newBean; // 消えた豆の位置に新しい豆を追加
-            io.emit("addNewBean", { index: index, left: newBean.left, top: newBean.top }); // 新しい豆の追加情報を全てのクライアントに送信
-        }, 1000); // アニメーション終了後に新しい豆を生成
+        socket.broadcast.emit("beanMoved", data);
     });
 
     socket.on("disconnect", () => {
         console.log("A user disconnected");
-        beans.forEach(bean => {
-            if (bean) {
-                bean.touchedBy = bean.touchedBy.filter(id => id !== socket.id);
-            }
-        });
     });
 });
 
@@ -80,3 +28,13 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// 初期豆の位置情報をサーバー側で管理
+let beans = [];
+for (let i = 0; i < 5; i++) {
+    beans.push({
+        left: Math.random() * 500, // 初期位置をランダムに設定
+        top: Math.random() * 500,
+    });
+}
+
