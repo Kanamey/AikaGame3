7f374ad7f8b909f8010ae4ce4e8a8965389ca075
Aika122 <yamashitaaika0122@gmail.com>
2024-12-08 19:21:10 +0900

    豆光る

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// 二人別々に豆を二人で移動できる
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
@@ -8,19 +7,55 @@ const io = socketIo(server);
 
 app.use(express.static("public"));
 
+let beans = [];
+for (let i = 0; i < 5; i++) {
+    beans.push({
+        left: Math.random() * 500,
+        top: Math.random() * 500,
+        touchedBy: [] // プレイヤーIDを格納する配列
+    });
+}
+
 io.on("connection", (socket) => {
     console.log("A user connected");
 
     // 初期状態の豆の位置情報を送信
     socket.emit("initializeBeans", beans);
 
+    // 豆が触られた時の処理
+    socket.on("beanTouched", (index) => {
+        if (!beans[index].touchedBy.includes(socket.id)) {
+            beans[index].touchedBy.push(socket.id);
+        }
+
+        // 二人が同時に触った場合に豆を光らせる
+        if (beans[index].touchedBy.length === 2) {
+            io.emit("beanGlow", index);
+        }
+    });
+
+    // 豆が離された時の処理
+    socket.on("beanReleased", (index) => {
+        beans[index].touchedBy = beans[index].touchedBy.filter(id => id !== socket.id);
+
+        // 光を止める
+        io.emit("beanStopGlow", index);
+    });
+
     // 豆の移動情報を他のクライアントにブロードキャスト
     socket.on("beanMoved", (data) => {
-        socket.broadcast.emit("beanMoved", data);
+        beans[data.index].left = data.left;
+        beans[data.index].top = data.top;
+        io.emit("beanMoved", data);
     });
 
     socket.on("disconnect", () => {
         console.log("A user disconnected");
+        beans.forEach(bean => {
+            if (bean) {
+                bean.touchedBy = bean.touchedBy.filter(id => id !== socket.id);
+            }
+        });
     });
 });
 
@@ -28,13 +63,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-// 初期豆の位置情報をサーバー側で管理
-let beans = [];
-for (let i = 0; i < 5; i++) {
-    beans.push({
-        left: Math.random() * 500, // 初期位置をランダムに設定
-        top: Math.random() * 500,
-    });
-}
-
