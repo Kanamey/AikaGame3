4920f7e7e8eceb3e062b4ab8cb2d11426af20017
Aika122 <yamashitaaika0122@gmail.com>
2024-12-11 17:19:23 +0900

    豆中点移動

--- index.js
+++ index.js
@@ -13,55 +13,47 @@ for (let i = 0; i < 5; i++) {
     beans.push({
         left: Math.random() * 500,
         top: Math.random() * 500,
-        touchedBy: [] // プレイヤーIDを格納する配列
+        touchedBy: []
     });
 }
 
 io.on("connection", (socket) => {
     console.log("A user connected");
 
-    // 初期状態の豆の位置情報を送信
+    // クライアントに初期豆を送信
     socket.emit("initializeBeans", beans);
 
-    // 豆が触られた時の処理
+    // 豆が触られたときの処理
     socket.on("beanTouched", (index) => {
         if (!beans[index].touchedBy.includes(socket.id)) {
             beans[index].touchedBy.push(socket.id);
         }
 
-        // 二人が同時に触っている場合に豆を光らせる
         if (beans[index].touchedBy.length === 2) {
-            io.emit("beanGlow", index);
+            io.emit("beanGlow", index); // 豆を光らせる
         }
     });
 
-    // 豆が離された時の処理
+    // 豆が離されたときの処理
     socket.on("beanReleased", (index) => {
         beans[index].touchedBy = beans[index].touchedBy.filter(id => id !== socket.id);
 
-        // 二人のタッチが解除された場合
         if (beans[index].touchedBy.length < 2) {
-            io.emit("beanStopGlow", index);
-
-            // 光が停止した豆を消滅させる
-            io.emit("beanDisappear", index);
-            beans.splice(index, 1); // サーバー側のリストから豆を削除
+            io.emit("beanStopGlow", index); // 豆の光を止める
         }
     });
 
-    // 二人が同時に触れている間、豆をカーソルの中点に移動
+    // カーソルの中点を更新
     socket.on("updateCursorMidpoint", (data) => {
         beans[data.index].left = data.x;
         beans[data.index].top = data.y;
-        io.emit("updateCursorMidpoint", data); // 全クライアントに中点情報を送信
+        io.emit("updateCursorMidpoint", data); // クライアントに中点情報を送信
     });
 
+    // 切断時の処理
     socket.on("disconnect", () => {
-        console.log("A user disconnected");
         beans.forEach(bean => {
-            if (bean) {
-                bean.touchedBy = bean.touchedBy.filter(id => id !== socket.id);
-            }
+            bean.touchedBy = bean.touchedBy.filter(id => id !== socket.id);
         });
     });
 });
