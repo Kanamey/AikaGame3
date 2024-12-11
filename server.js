f41a87e6eb1de2b727cf28716d6c0a42aab27652
Aika122 <yamashitaaika0122@gmail.com>
2024-12-11 20:00:30 +0900

    計算をサーバーに移動

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+// 豆の初期位置データ
+let beans = [];
+for (let i = 0; i < 5; i++) {
+    beans.push({
+        id: i,
+        x: Math.random() * 500,
+        y: Math.random() * 500,
+        touchedBy: []
+    });
+}
+
+// クライアントとの通信
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    // 初期豆の位置を送信
+    socket.emit("initializeBeans", beans);
+
+    // クライアントからの豆移動リクエスト
+    socket.on("moveBean", ({ id, newX, newY }) => {
+        const bean = beans.find(b => b.id === id);
+        if (bean) {
+            bean.x = newX;
+            bean.y = newY;
+            io.emit("updateBeanPosition", { id, newX, newY });
+        }
+    });
+
+    // 豆の中点移動リクエスト
+    socket.on("updateMidpoint", ({ id, x1, y1, x2, y2 }) => {
+        const bean = beans.find(b => b.id === id);
+        if (bean) {
+            // 中点を計算
+            bean.x = (x1 + x2) / 2;
+            bean.y = (y1 + y2) / 2;
+
+            // 全クライアントに通知
+            io.emit("updateBeanPosition", { id, newX: bean.x, newY: bean.y });
+        }
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+// サーバー起動
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
