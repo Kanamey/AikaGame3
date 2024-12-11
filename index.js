f0ee8bfa81100c72e4a33208b29e27fd3d7dcbe2
Aika122 <yamashitaaika0122@gmail.com>
2024-12-11 19:10:22 +0900

    整理２

--- index.js
+++ index.js
@@ -7,23 +7,42 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // ゲーム状態を管理する変数
-let gameState = {};
+let gameState = {
+  blocks: [
+    { id: 1, x: 1, y: 1 }, // ブロックの初期位置
+    { id: 2, x: 3, y: 2 },
+  ],
+  goals: [
+    { x: 5, y: 5 }, // ゴール位置
+    { x: 6, y: 5 },
+  ],
+};
 
+// サーバーのイベント
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
-  // クライアントからデータを受信
-  socket.on("playerAction", (data) => {
-    // サーバーで状態を更新
-    gameState = { ...gameState, [socket.id]: data };
-    // 他のクライアントに状態を送信
-    socket.broadcast.emit("updateGameState", gameState);
+  // 初期状態を送信
+  socket.emit("initGameState", gameState);
+
+  // クライアントからブロック移動の要求を受け取る
+  socket.on("moveBlock", ({ blockId, direction }) => {
+    const block = gameState.blocks.find((b) => b.id === blockId);
+
+    if (block) {
+      // 移動の計算
+      if (direction === "up") block.y -= 1;
+      if (direction === "down") block.y += 1;
+      if (direction === "left") block.x -= 1;
+      if (direction === "right") block.x += 1;
+
+      // 全てのクライアントに状態を送信
+      io.emit("updateGameState", gameState);
+    }
   });
 
-  // 切断時の処理
   socket.on("disconnect", () => {
     console.log("A user disconnected:", socket.id);
-    delete gameState[socket.id];
   });
 });
 
