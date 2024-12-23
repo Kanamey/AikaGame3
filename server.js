dff6f62cb7ec5ef299e9d49b21c38d2941c1d8f3
Aika122 <yamashitaaika0122@gmail.com>
2024-12-23 16:43:25 +0900

    スタートボタン追加

--- server.js
+++ server.js
@@ -64,6 +64,13 @@ io.on("connection", (socket) => {
     console.log(`Player connected: ${socket.id}`);
     players[socket.id] = { x: 0, y: 0 };
 
+    // スタートイベントを受け取る
+    socket.on("startGame", () => {
+        console.log("ゲーム開始リクエストを受信しました。");
+        // 全てのクライアントにゲーム開始を通知
+        io.emit("gameStarted");
+    });
+    
     // socket.emit("initializeBeans", beans);
 
     // プレイヤーのマウス位置を受信
